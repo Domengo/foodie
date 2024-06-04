b2428b7a3dae6dbfdfc4064bbfde15cd6c871aab
Domengo <dominicsengo@gmail.com>
2024-06-04 05:47:01 +0300

    chore: Update dependencies and add Hive Flutter package

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,12 +1,13 @@
 import 'package:http/http.dart' as http;
 import '../models/food_item.dart';
+import 'dart:convert';
 
 class APIService {
   static const String baseUrl = 'https://spoonacular.com/food-api';
-  
+
   Future<List<FoodItem>> fetchFoodItems() async {
     final response = await http.get(Uri.parse('$baseUrl/food-items'));
-    
+
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body);
       return data.map((item) => FoodItem.fromJson(item)).toList();
@@ -14,6 +15,6 @@ class APIService {
       throw Exception('Failed to fetch food items');
     }
   }
-  
+
   // Other API-related methods...
 }
